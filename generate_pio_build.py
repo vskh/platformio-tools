b1f3475add62e753a373d37113fd6ebd613d7419
Vadym S. Khondar <vadym@khondar.name>
2020-06-06 19:47:12 +0100

    Build script further, add some tests.

--- generate_pio_build.py
+++ generate_pio_build.py
@@ -7,10 +7,81 @@ specification file (platform.txt).
 
 import sys
 import re
-from typing import Dict
+import shlex
+from typing import List, Dict, Callable, AnyStr, Any
+
+RECIPE_IDS = {
+    'c': 'recipe.c.o.pattern',          # C compiling
+    'c++': 'recipe.cpp.o.pattern',      # C++ compiling
+    'as': 'recipe.S.o.pattern',         # asm compiling
+    'ld': 'recipe.c.combine.pattern',   # linking
+    'ar': 'recipe.ar.pattern'           # creating archive
+}
 
 
-def expand(def_str: str, def_index: Dict[str, str]) -> str:
+def extract_v_tokens(tokens: List[AnyStr], match: Callable[[AnyStr], re.Match]) -> List[AnyStr]:
+    result = []
+    for token in tokens:
+        m = match(token)
+        if m is not None:
+            v = m.group(1)
+            result.append(v)
+    return result
+
+
+def extract_kv_tokens(tokens: List[AnyStr], match: Callable[[AnyStr], re.Match]) -> Dict[AnyStr, AnyStr]:
+    result = {}
+    for token in tokens:
+        matches = match(token)
+        if matches is not None:
+            k, v = matches.groups()
+            result[k] = v if v else None
+    return result
+
+
+def extract_cppdefines(tokens: List[AnyStr]) -> Dict[AnyStr, AnyStr]:
+    return extract_kv_tokens(tokens, lambda t: re.search("^-D(\w+)=?(.*?)?$", t))
+
+
+def extract_cpppath(tokens: List[AnyStr]) -> List[AnyStr]:
+    return extract_v_tokens(tokens, lambda t: re.search("^-I(.*?)$", t))
+
+
+def extract_libs(cmdline: AnyStr) -> List[AnyStr]:
+    return extract_v_tokens(cmdline, lambda t: re.search("^-l(.*?)$", t))
+
+
+def extract_build_flags(recipes: Dict[AnyStr, List[AnyStr]]) -> Dict[AnyStr, Any]:
+    extracted_flags = {
+        'CCFLAGS': set(),       # C/C++ common compiler flags
+        'CFLAGS': set(),        # C compiler flags
+        'CPPDEFINES': set(),    # C preprocessor flags
+        'CPPPATH': set(),       # C preprocessor includes search path
+        'CXXFLAGS': set(),      # C++ compiler flags
+        'LIBPATH': set(),       # library search path
+        'LIBS': set(),          # libraries to link with
+        'LINKFLAGS': set()      # linker flags
+    }
+
+    # collect C flags
+    c_recipe = recipes[RECIPE_IDS['c']]
+    extracted_flags['CPPDEFINES'].add(extract_cppdefines(c_recipe))
+    extracted_flags['CPPPATH'].add(extract_cpppath(c_recipe))
+
+    # collect C++ flags
+    cxx_recipe = recipes[RECIPE_IDS['c++']]
+    extracted_flags['CPPDEFINES'].add(extract_cppdefines(cxx_recipe))
+    extracted_flags['CPPPATH'].add(extract_cpppath(cxx_recipe))
+
+    # collect libraries
+    ld_recipe = recipes[RECIPE_IDS['ld']]
+    extracted_flags['LIBS'].add(extract_libs(ld_recipe))
+    extracted_flags['LINKFLAGS'].add()
+
+    return extracted_flags
+
+
+def expand(def_str: AnyStr, def_index: Dict[AnyStr, AnyStr]) -> AnyStr:
     class fmt_dict(dict):
         def __missing__(self, key):
             return f"{{{key}}}"
@@ -18,15 +89,17 @@ def expand(def_str: str, def_index: Dict[str, str]) -> str:
     return def_str.format_map(fmt_dict(def_index))
 
 
-def extract_expanded_recipes(platform_def: Dict[str, str]) -> Dict[str, str]:
-    recipes = ['recipe.c.o.pattern',        # C compiling
-               'recipe.cpp.o.pattern',      # C++ compiling
-               'recipe.S.o.pattern',        # asm compiling
-               'recipe.c.combine.pattern',  # linking
-               'recipe.ar.pattern']         # creating archive
+def extract_expanded_recipes(platform_def: Dict[AnyStr, AnyStr]) -> Dict[AnyStr, List[AnyStr]]:
+    result = {}
+    recipes = RECIPE_IDS.values()
+    for recipe in recipes:
+        expanded_recipe = expand(platform_def[recipe], platform_def)
+        result[recipe] = shlex.split(expanded_recipe)
+
+    return result
 
 
-def read_platform_def(platform_def_path: str) -> Dict[str, str]:
+def read_platform_def(platform_def_path: AnyStr) -> Dict[AnyStr, AnyStr]:
     result = {}
     with open(platform_def_path) as platform:
         for l in platform:
@@ -40,7 +113,9 @@ def read_platform_def(platform_def_path: str) -> Dict[str, str]:
 
             k, v = l.split("=", 1)
 
-            result[k] = v
+            def dot_filter(m): return m.group(0).replace(".", "_")
+
+            result[k.replace(".", "_")] = re.sub("\{[^{}]+\}", dot_filter, v)
 
     return result
 
@@ -61,8 +136,9 @@ def main(argv):
         script = None
 
     platform_def = read_platform_def(platform)
-
-    print(platform_def)
+    recipes = extract_expanded_recipes(platform_def)
+    build_flags = extract_build_flags(recipes)
+    print(build_flags)
 
 
 if __name__ == "__main__":
--- generate_pio_build_test.py
+++ generate_pio_build_test.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 import unittest
-from generate_pio_build import expand
+import re
+import generate_pio_build as gpb
+
+
+class ReadPlatformDefTests(unittest.TestCase):
+    def test_expands_dots_in_format_placeholders(self):
+        platform_def = gpb.read_platform_def("platform_test.txt")
+
+        test_key = "compiler_S_flags"
+        self.assertIn(test_key, platform_def)
+
+        val = platform_def[test_key].strip()
+
+        self.assertEqual(
+            val, '-c -x assembler-with-cpp {compiler_libstm_c_flags} -DTRANSACTION_QUEUE_SIZE_SPI=2 -D__CORTEX_M4 -DUSB_STM_HAL -DARM_MATH_CM4 -D__FPU_PRESENT=1 -DUSBHOST_OTHER -D__MBED_CMSIS_RTOS_CM -D__CMSIS_RTOS')
 
 
 class ExpandTests(unittest.TestCase):
@@ -9,16 +23,98 @@ class ExpandTests(unittest.TestCase):
 
     def test_expands_single(self):
         var = 'test1'
-        expansion = expand("{{{}}}".format(var), self.test_def_index)
+        expansion = gpb.expand("{{{}}}".format(var), self.test_def_index)
         self.assertEqual(expansion, self.test_def_index[var])
 
     def test_expands_multiple(self):
-        expansion = expand("test = {test1} {test2}", self.test_def_index)
+        expansion = gpb.expand("test = {test1} {test2}", self.test_def_index)
         self.assertEqual(expansion, "test = 1 2")
 
     def test_leaves_unknown_as_is(self):
-        expansion = expand("test = {test3} {test5}", self.test_def_index)
+        expansion = gpb.expand("test = {test3} {test5}", self.test_def_index)
         self.assertEqual(expansion, "test = 3 {test5}")
+
+
+class ExtractExpandedRecipesTests(unittest.TestCase):
+    test_platform_def = {
+        'test1': "gcc",
+        'test2': "g++",
+        'test3': "as",
+        'test4': "ld",
+        'test5': "ar",
+        'recipe.c.o.pattern': "{test1} myfile",
+        'recipe.cpp.o.pattern': "{test2} myfile",
+        'recipe.S.o.pattern': "{test3} myfile",
+        'recipe.c.combine.pattern': "{test4} myfile",
+        'recipe.ar.pattern': "{test5} myfile"
+    }
+
+    def test_works(self):
+        expected_recipes = {
+            'recipe.c.o.pattern': ["gcc", "myfile"],
+            'recipe.cpp.o.pattern': ["g++", "myfile"],
+            'recipe.S.o.pattern': ["as", "myfile"],
+            'recipe.c.combine.pattern': ["ld", "myfile"],
+            'recipe.ar.pattern': ["ar", "myfile"]
+        }
+        recipes = gpb.extract_expanded_recipes(self.test_platform_def)
+        self.assertDictEqual(recipes, expected_recipes)
+
+
+class ExtractorsTests(unittest.TestCase):
+    def test_extract_kv_tokens(self):
+        expected_result = {
+            'TEST_NO_VALUE': None,
+            'TEST': 'VALUE'
+        }
+        result = gpb.extract_kv_tokens(
+            ["TEST_NO_VALUE", "TEST=VALUE"], lambda t: re.search("^([A-Z_]+)=?([A-Z]+)?$", t))
+        self.assertEqual(result, expected_result)
+
+    def test_extract_v_tokens(self):
+        expected_result = ["A", "B", "C"]
+        result = gpb.extract_v_tokens(
+            ["opt:A", "opt:B", "opt:C"], lambda t: re.search("^opt:([A-Z])$", t))
+        self.assertEqual(result, expected_result)
+
+    def test_extract_cppdefines(self):
+        expected_defines = {
+            'USBCON': None,
+            'USB_MANUFACTURER': '{build.usb_manufacturer}'
+        }
+        defines = gpb.extract_cppdefines(
+            ["-DUSBCON", "-DUSB_MANUFACTURER={build.usb_manufacturer}"])
+        self.assertDictEqual(defines, expected_defines)
+
+    def test_extract_cpppath(self):
+        expected_cpppaths = [
+            "{build.system.path}",
+            "/c/Program Files/mbed-os",
+            "."
+        ]
+        cpppaths = gpb.extract_cpppath(
+            ["-I{build.system.path}", "-I/c/Program Files/mbed-os", "-I."])
+        self.assertListEqual(cpppaths, expected_cpppaths)
+
+    def test_extract_libs(self):
+        expected_libs = [
+            "devkit-sdk-core-lib",
+            "wlan",
+            "stsafe",
+            "devkit-sdk-core-lib",
+            "m",
+            "stdc++"
+        ]
+        libs = gpb.extract_libs([
+            "test should not be in result",
+            "-ldevkit-sdk-core-lib",
+            "-lwlan",
+            "-lstsafe",
+            "-ldevkit-sdk-core-lib",
+            "-lm",
+            "-lstdc++"
+        ])
+        self.assertListEqual(libs, expected_libs)
 
 
 if __name__ == "__main__":
